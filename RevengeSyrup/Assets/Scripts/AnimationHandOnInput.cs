3c946cdc27fa89db8b179c261cd96e8ab565d186
SparkyPX <pierrelucproulx@gmail.com>
2025-04-02 17:16:17 -0400

    push

--- RevengeSyrup/Assets/Scripts/AnimationHandOnInput.cs
+++ RevengeSyrup/Assets/Scripts/AnimationHandOnInput.cs
@@ -6,6 +6,8 @@ public class AnimationHandOnInput : MonoBehaviour
 {
 
     public InputActionProperty pinchAnimation;
+    public InputActionProperty gripAnimationAction;
+    public Animator handAnimator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,7 +18,9 @@ public class AnimationHandOnInput : MonoBehaviour
     void Update()
     {
         float triggerValue = pinchAnimation.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
 
-        Debug.Log(triggerValue);
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
     }
 }
